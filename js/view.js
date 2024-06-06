2195a64caf194ef8b188ca060262d7f39944c2b8
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2024-06-06 12:23:24 +0200

    lagt til knapp ikke stil

--- js/view.js
+++ js/view.js
@@ -4,9 +4,10 @@ function initialView() {
     document.getElementById('void').innerHTML = /*html*/ `
     <button onclick="filterView()" style="height: 75px; width: 100px; font-size: large;">Filter</button>
     <h1>Choose a random FFXIV job!</h1>
-        <button class="jobButton" onClick="resultView()"
-                style="height: 250px; width: 250px; font-size: x-large; ">Find a
-                job</button>
+    <div id="startButtons">    
+    <button class="jobButton" onClick="resultView()" style="height: 250px; width: 250px; font-size: x-large; ">Find a job</button>
+    <button style="height: 250px; width: 250px; font-size: x-large; " class="jobButton" onClick="roleView()">Find a role</button>
+    </div>
     `
 }
 
